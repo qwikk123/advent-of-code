cab2ed4989300ae4b4c83c51fe11ec9fdeb1d049
qwikk <44299295+qwakk@users.noreply.github.com>
2023-12-04 19:39:19 +0100

    refactor for day 1 2022

--- AoC2022/Day1/Day1.kt
+++ AoC2022/Day1/Day1.kt
@@ -4,25 +4,13 @@ import java.io.File
 import kotlin.math.max
 
 fun main() {
-    // PART 1
     val input = File("AoC2022/Day1/input.txt").readLines()
     var highestCalories = 0
     var currentCalories = 0
+    val top3 = mutableListOf(0,0,0)
     input.forEach {
         if (it == "") {
             highestCalories = max(currentCalories, highestCalories)
-            currentCalories = 0
-        }
-        else currentCalories += it.toInt()
-    }
-    println("PART 1: $highestCalories")
-
-
-    //PART 2
-    val top3 = mutableListOf(0,0,0)
-    currentCalories = 0
-    input.forEach {
-        if (it == "") {
             top3.add(currentCalories)
             top3.sortDescending()
             top3.removeLast()
@@ -30,5 +18,6 @@ fun main() {
         }
         else currentCalories += it.toInt()
     }
+    println("PART 1: $highestCalories")
     println("PART 2: ${top3.sum()}")
 }
