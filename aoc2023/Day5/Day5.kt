eeeb84dd304842f7dc6eb210529f7f0411f298b8
qwikk <44299295+qwakk@users.noreply.github.com>
2023-12-06 12:57:54 +0100

    Day 5 part 2

--- aoc2023/Day5/Day5.kt
+++ aoc2023/Day5/Day5.kt
@@ -1,6 +1,8 @@
 package aoc2023.Day5
 
 import java.io.File
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
     val input = File("aoc2023/Day5/input.txt").readText().split("\n\n")
@@ -15,72 +17,43 @@ fun main() {
     val ranges = input.first().split(" ").drop(1)
         .map(String::toLong)
         .chunked(2)
-        .map { it.first()..<it.first()+it.last() }
+        .map { it.first() to it.last() }
 
     val part1 = findLocations(part1Seeds, groups).values.min()
     println(part1)
 
     val test = part2(ranges, groups)
-    test.forEachIndexed { i, it ->
-        println("----- INDEX: $i")
-        println(it)
-    }
-    println(test.flatten().minOf { it.first })
+    println(test)
 }
 
-private fun part2(ranges: List<LongRange>, groups: List<List<List<Long>>>): List<List<LongRange>> {
-    return ranges.mapIndexed {index, range ->
+private fun part2(ranges: List<Pair<Long, Long>>, groups: List<List<List<Long>>>): Long {
+    return ranges.flatMap {range ->
         groups.fold(listOf(range)) { ranges, maps ->
-            if (index == 6) {
-                println(ranges)
-                println(maps)
-            }
-
-            val splitRanges = mutableListOf<LongRange>()
+            val splitRanges = mutableListOf<Pair<Long, Long>>()
             val unChangedRanges = ranges.toMutableList()
+
             maps.forEach { mapRange ->
                 val (to, from, length) = mapRange
-                val fromRange = from..<from+length
-                val toRange = to..<to+length
-                val offset = toRange.first-fromRange.first
+                val fromEnd = from+length
 
                 val rangesToSplit = unChangedRanges.filter {
-                    it.first <= fromRange.last && fromRange.first <= it.last
+                    it.first <= fromEnd && from <= it.first+it.second
                 }
                 unChangedRanges.removeAll { it in rangesToSplit }
 
                 rangesToSplit.forEach {
-                    //its end overlaps
-                    if (it.first < fromRange.first && it.last <= fromRange.last) {
-                        unChangedRanges.add(it.first..<from)
-                        val newRange = fromRange.first.. it.last
-                        splitRanges.add(newRange.first+offset .. newRange.last+offset)
-                    }
-                    //its start overlaps
-                    else if (it.first >= fromRange.first && it.last > fromRange.last) {
-                        unChangedRanges.add(fromRange.last()+1 .. it.last)
-                        val newRange = it.first..fromRange.last
-                        splitRanges.add(newRange.first+offset..newRange.last+offset)
-                    }
-                    //it is around from
-                    else if (it.first < fromRange.first && it.last > fromRange.last) {
-                        unChangedRanges.add(it.first..<fromRange.first)
-                        unChangedRanges.add(fromRange.last+1..it.last)
-                        splitRanges.add(toRange)
-                    }
-                    //from is around it
-                    else {
-                        unChangedRanges.add(it.first+offset..it.last+offset)
-                    }
+                    val start = it.first
+                    val end = it.first+it.second
+
+                    splitRanges += (to + max(start, from) - from) to min(end, fromEnd) - max(start, from)
+                    if (start < from) unChangedRanges += start to (from - start)
+                    if (end > fromEnd) unChangedRanges += fromEnd to (end-fromEnd)
+
                 }
-
-                if (index == 6) println("rangestosplit $rangesToSplit in $mapRange -> $fromRange")
-
             }
-
             splitRanges+unChangedRanges
         }
-    }
+    }.minOf { it.first }
 }
 
 private fun findLocations(seeds: Sequence<Long>, maps: List<List<List<Long>>>): MutableMap<Long, Long> {
---
+++ aoc2023/Day6/Day6.kt
@@ -0,0 +1,9 @@
+package aoc2023.Day6
+
+import java.io.File
+
+fun main() {
+    val input = File("aoc2023/Day6/input.txt").readLines()
+
+
+}
