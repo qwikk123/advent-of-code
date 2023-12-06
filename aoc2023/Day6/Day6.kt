23b6def8a4b37c5b376e89650e1ce1c7687ad5c8
qwikk <44299295+qwakk@users.noreply.github.com>
2023-12-06 15:00:53 +0100

    Day 6 part 1 and 2

--- aoc2023/Day6/Day6.kt
+++ aoc2023/Day6/Day6.kt
@@ -4,6 +4,24 @@ import java.io.File
 
 fun main() {
     val input = File("aoc2023/Day6/input.txt").readLines()
+    val times = "\\d+".toRegex().findAll(input[0]).map { it.value }
+    val distances = "\\d+".toRegex().findAll(input[1]).map { it.value }
+    val races = times.map(String::toInt).zip(distances.map(String::toInt))
 
+    val part1 = races.map {
+        val (time, record) = it
+        var n = 0
+        for (i in time downTo 1) if (i*(time-i) <= record) n++ else break
+        (time+1)-(n*2)
+    }.reduce { acc, i -> acc*i }
 
+    val part2 = {
+        val time = times.reduce{acc, s -> acc+s }.toLong()
+        val record = distances.reduce{acc, s -> acc+s }.toLong()
+        var n = 0
+        for (i in time downTo 1) if (i*(time-i) <= record) n++ else break
+        (time+1)-(n*2)
+    }
+    println(part1)
+    println(part2())
 }
