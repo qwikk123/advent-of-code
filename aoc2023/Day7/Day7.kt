984798ddee82bdad90e539f6de3d8a3ed612cb8b
qwikk <44299295+qwakk@users.noreply.github.com>
2023-12-07 13:30:27 +0100

    Day 7 part 2 removed debug print

--- aoc2023/Day7/Day7.kt
+++ aoc2023/Day7/Day7.kt
@@ -43,8 +43,6 @@ fun main() {
         { pair -> valueMap[pair.first[4].toString()] }
     ))
 
-    sorted.forEach { println(it) }
-
     val sum = sorted.mapIndexed{index, pair -> pair.second.toLong()*(index+1) }.sum()
     println(sum)
 }
