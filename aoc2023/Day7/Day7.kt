9313604a9c402f77dab2195f091772d0d8385c27
qwikk <44299295+qwakk@users.noreply.github.com>
2023-12-07 13:29:59 +0100

    Day 7 part 2

--- aoc2023/Day7/Day7.kt
+++ aoc2023/Day7/Day7.kt
@@ -9,12 +9,23 @@ fun main() {
         Pair(split.first(), split.last().toInt())
     }
     val valueMap = mapOf("2" to 2,"3" to 3,"4" to 4,"5" to 5,"6" to 6,"7" to 7,"8" to 8,"9" to 9,
-        "T" to 10, "J" to 11, "Q" to 12, "K" to 13, "A" to 14)
+        "T" to 10, "J" to 1, "Q" to 12, "K" to 13, "A" to 14)
 
     val sorted = hands.sortedWith(compareBy(
         { pair ->
             val (hand, _) = pair
-            val type = hand.groupBy { it }
+            val type = hand.groupBy { it }.toMutableMap()
+
+            //PART 2
+            if (type.containsKey('J') && type.size != 1) {
+                val nJ = type['J']!!.size
+                type.remove('J')
+                val nX = type.values.maxBy { it.size }.first()
+                val tempList = type[nX]!!.toMutableList()
+                repeat(nJ){ tempList.add(nX) }
+                type[nX] = tempList
+            }
+
             val typeValue =
                 if (type.size == 1) 6
                 else if (type.size == 2 && type.any { it.value.size==4 }) 5
@@ -31,6 +42,8 @@ fun main() {
         { pair -> valueMap[pair.first[3].toString()] },
         { pair -> valueMap[pair.first[4].toString()] }
     ))
+
+    sorted.forEach { println(it) }
 
     val sum = sorted.mapIndexed{index, pair -> pair.second.toLong()*(index+1) }.sum()
     println(sum)
