a2a53e8c31a3ffb31dd17dbf670d10355452d2a3
qwikk <44299295+qwakk@users.noreply.github.com>
2023-12-07 13:06:06 +0100

    Day 7 part 1

--- aoc2023/Day6/Day6.kt
+++ aoc2023/Day6/Day6.kt
@@ -8,19 +8,21 @@ fun main() {
     val distances = "\\d+".toRegex().findAll(input[1]).map { it.value }
     val races = times.map(String::toInt).zip(distances.map(String::toInt))
 
-    val part1 = races.map {
-        val (time, record) = it
-        var n = 0
-        for (i in time downTo 1) if (i*(time-i) <= record) n++ else break
-        (time+1)-(n*2)
-    }.reduce { acc, i -> acc*i }
+    val part1 = races
+        .map {
+            val (time, record) = it
+            var n = 0
+            for (i in time downTo 1) if (i * (time - i) <= record) n++ else break
+            (time + 1) - (n * 2)
+        }
+        .reduce { acc, i -> acc * i }
 
     val part2 = {
-        val time = times.reduce{acc, s -> acc+s }.toLong()
-        val record = distances.reduce{acc, s -> acc+s }.toLong()
+        val time = times.reduce { acc, s -> acc + s }.toLong()
+        val record = distances.reduce { acc, s -> acc + s }.toLong()
         var n = 0
-        for (i in time downTo 1) if (i*(time-i) <= record) n++ else break
-        (time+1)-(n*2)
+        for (i in time downTo 1) if (i * (time - i) <= record) n++ else break
+        (time + 1) - (n * 2)
     }
     println(part1)
     println(part2())
---
+++ aoc2023/Day7/Day7.kt
@@ -0,0 +1,37 @@
+package aoc2023.Day7
+
+import java.io.File
+
+fun main() {
+    val input = File("aoc2023/Day7/input.txt").readLines()
+    val hands = input.map {
+        val split = it.split(" ")
+        Pair(split.first(), split.last().toInt())
+    }
+    val valueMap = mapOf("2" to 2,"3" to 3,"4" to 4,"5" to 5,"6" to 6,"7" to 7,"8" to 8,"9" to 9,
+        "T" to 10, "J" to 11, "Q" to 12, "K" to 13, "A" to 14)
+
+    val sorted = hands.sortedWith(compareBy(
+        { pair ->
+            val (hand, _) = pair
+            val type = hand.groupBy { it }
+            val typeValue =
+                if (type.size == 1) 6
+                else if (type.size == 2 && type.any { it.value.size==4 }) 5
+                else if ((type.size == 2 && type.any { it.value.size==3 })) 4
+                else if (type.size == 3 && type.any { it.value.size==3 }) 3
+                else if (type.size == 3 && type.filter { it.value.size ==2 }.size==2) 2
+                else if (type.size == 4) 1
+                else 0
+            typeValue
+        },
+        { pair -> valueMap[pair.first[0].toString()] },
+        { pair -> valueMap[pair.first[1].toString()] },
+        { pair -> valueMap[pair.first[2].toString()] },
+        { pair -> valueMap[pair.first[3].toString()] },
+        { pair -> valueMap[pair.first[4].toString()] }
+    ))
+
+    val sum = sorted.mapIndexed{index, pair -> pair.second.toLong()*(index+1) }.sum()
+    println(sum)
+}
