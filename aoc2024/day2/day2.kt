47d3f9de79070319aad9ffe4651dbb7d57dde7de
qwikk123 <44299295+qwikk123@users.noreply.github.com>
2024-12-02 19:26:37 +0100

    day2 part 1 and 2

---
+++ aoc2024/day2/day2.kt
@@ -0,0 +1,21 @@
+package aoc2024.day2
+
+import java.io.File
+
+fun main() {
+    val input = File("aoc2024/day2/input").readLines().map { it.split(" ").map { it.toInt() } }
+
+    val isSafe = { r: List<Int>  ->
+        val diffs = r.zipWithNext { a, b -> b - a }
+        val range = if (diffs.first() > 0) 1..3 else -3..-1
+        diffs.all { it in range }
+    }
+    val isSafeDampened = { r: List<Int> -> r.indices.any { remove -> isSafe(r.filterIndexed { i, _ -> i != remove }) } }
+
+    val part1 = input.count { isSafe(it) }
+    val part2 = input.count { isSafeDampened(it) }
+
+    println(part1)
+    println(part2)
+}
+
